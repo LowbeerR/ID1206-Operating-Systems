30923ef889fda00523e1cb480e77e1486393dc46
Emil Göransson <emilgo@kth.se>
2023-11-01 17:47:19 +0100

    Update main.c

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/wait.h>
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    pid_t pid;
+    int pip[2];
+    pipe(pip);
+    pid = fork();
+    if (pid == 0) {
+        close(pip[0]);
+        dup2(pip[1], STDOUT_FILENO);
+        execl("/bin/ls", "ls", "/", NULL);
+        return 0;
+    }
+    if (pid > 0) {
+        wait(NULL);
+        close(pip[1]);
+        dup2(pip[0], STDIN_FILENO);
+        execl("/usr/bin/wc", "wc", "-l", NULL);
+        return 0;
+    }
 }
