277d5afdc27705cc01a5788063ac68060dabea69
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-11-03 11:17:21 +0100

    Setup för part 2

---
+++ Lab-1/part2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/wait.h>
+
+int main(){
+    printf("%s", "Hello World");
+    return 0;
+}
