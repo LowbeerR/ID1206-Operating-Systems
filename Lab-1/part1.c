65065872716768391afe9f5b9ce286b13edaa63d
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-11-03 11:37:23 +0100

    random

--- Lab-1/part1.c
+++ Lab-1/part1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h>
--- Lab-1/part2.c
+++ Lab-1/part2.c
@@ -4,8 +4,30 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/wait.h>
+#include <sys/fcntl.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+
 
 int main(){
-    printf("%s", "Hello World");
+    struct mq_attr {
+        long mq_flags; // Message queue description flags
+        long mq_maxmsg; // Maximum number of messages on queue
+        long mq_msgsize; // Maximum message size (in bytes)
+        long mq_curmsgs; // Number of messages currently in queue
+    };
+
+    pid_t pid;
+    mqd_t mq_open();
+    pid = fork();
+    if (pid == 0) {
+        //first process = child process
+        //read context of file and pass it
+    }
+    if (pid > 0){
+        wait(NULL);
+    }
+
+
     return 0;
 }
